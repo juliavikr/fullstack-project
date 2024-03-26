027b9c988c72f0ab244b68384ca9d939875b108d
andreaamundsen <andramun@stud.ntnu.no>
2024-03-26 14:35:48 +0100

    Lagt til Spring Boot backend-oppsett

---
+++ quizbank/src/main/java/no/ntnu/idatt2105/quizbank/QuizBankWebApplication.java
@@ -0,0 +1,13 @@
+package no.ntnu.idatt2105.quizbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizBankWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizBankWebApplication.class, args);
+	}
+
+}
---
+++ quizbank/src/test/java/no/ntnu/idatt2105/quizbank/QuizBankWebApplicationTests.java
@@ -0,0 +1,13 @@
+package no.ntnu.idatt2105.quizbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizBankWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
