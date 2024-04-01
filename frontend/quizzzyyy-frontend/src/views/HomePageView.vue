1f76398c39373f8e18e1e345518062dfb18092b0
Julia Vik Remøy <juliavr@stud.ntnu.no>
2024-04-01 10:48:40 +0200

    homepageview css done

--- frontend/quizzzyyy-frontend/src/views/HomePageView.vue
+++ frontend/quizzzyyy-frontend/src/views/HomePageView.vue
@@ -35,7 +35,6 @@ const createQuiz = () => {
   align-items: center; /* Senterer knappene horisontalt */
   width: 100%; /* Setter bredden på knappene til å passe containeren */
   gap: 30px; /* Gir litt plass mellom knappene */
-  padding-top: 70px; /* Denne må være høy nok til å skyve innholdet ned fra navbar */
   margin-top: 70px; /* Eller bruk margin-top istedenfor padding-top om nødvendig */
   align-items: center;
 }
