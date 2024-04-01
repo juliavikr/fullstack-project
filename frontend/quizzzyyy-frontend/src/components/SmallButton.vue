8c0361da775c69e56acb7ce2333a8a01b4477e3a
Julia Vik Remøy <juliavr@stud.ntnu.no>
2024-04-01 16:38:18 +0200

    SmallButton component done

--- frontend/quizzzyyy-frontend/src/components/SmallButton.vue
+++ frontend/quizzzyyy-frontend/src/components/SmallButton.vue
@@ -1,39 +1,37 @@
 <template>
-  <button class="small-button" :class="{ active: isActive }" @click="toggleActive">
+  <button class="small-button" :class="{ active: isActive }" @click="$emit('click')">
     <slot></slot>
   </button>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-
 defineProps({
-  label: String
+  isActive: Boolean
 })
-
-const isActive = ref(false)
-
-const toggleActive = () => {
-  isActive.value = !isActive.value
-  // Her kan du også bruke en hendelse hvis du vil oppdatere en høyere tilstand
-}
 </script>
 
 <style scoped>
 .small-button {
+  display: flex; /* Setter display til flex for å tillate flexbox egenskaper */
+  align-items: center; /* Sentrerer barn vertikalt i flex container */
+  justify-content: center; /* Sentrerer barn horisontalt i flex container */
   padding: 10px 15px; /* Gir knappene størrelse og plass */
-  width: 70px; /* Standard bredde på knappene */
-  height: 20px; /* Standard høyde på knappene */
+  min-width: 70px; /* Minimumsbredde på knappene */
+  height: 40px; /* Justert høyde for bedre estetikk */
   font-size: 1rem; /* Passende skriftstørrelse */
   border-radius: 5px; /* Avrundede hjørner */
   cursor: pointer; /* Endrer musepekeren til en peker */
-  background-color: #ff8bc3; /* Pink color for the buttons */
+  background-color: #ff8bc3; /* Rosa farge for knappene */
   margin: 0 5px; /* Liten margin mellom knappene */
-  text-align: center; /* Sentrerer tekst i knappen */
+  border: none; /* Fjerner standard kant */
   transition: background-color 0.3s; /* Gjør en myk overgang for bakgrunnsfarge ved hover */
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  text-decoration: none; /* Fjerner tekstdekorasjon som kan komme fra <a> elementer */
 }
 .small-button.active {
-  border: #f451a0;
+  background-color: #f362a8; /* Mørkere rosa farge for aktive knapper */
+}
+
+.small-button:hover {
+  background-color: #f362a8; /* Mørkere rosa farge for hover-effekt */
 }
 </style>
