f0f725e571a76db51499659ac586a7a4592a92ea
Julia Vik Remøy <juliavr@stud.ntnu.no>
2024-04-01 17:54:38 +0200

    ImageIcone implemented

---
+++ frontend/quizzzyyy-frontend/src/components/icons/ImageIcon.vue
@@ -0,0 +1,8 @@
+<template>
+  <svg width="72" height="55" viewBox="0 0 72 55" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect x="0.5" y="0.5" width="71" height="54" rx="4.5" fill="white" stroke="black" />
+    <line x1="0.736319" y1="33.5752" x2="29.7363" y2="15.5752" stroke="black" />
+    <line x1="29.335" y1="15.6288" x2="70.335" y2="52.6288" stroke="black" />
+    <circle cx="58.5" cy="13.5" r="4" fill="white" stroke="black" />
+  </svg>
+</template>
