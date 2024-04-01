0d25aac0e17a090fdf5dbbfb9c32a4671643d4e2
Julia Vik Remøy <juliavr@stud.ntnu.no>
2024-04-01 12:14:29 +0200

    QuestionCreationBox component created

---
+++ frontend/quizzzyyy-frontend/src/components/QuestionCreationBox.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="question-box">
+    <input type="text" placeholder="Question" />
+    <input type="text" placeholder="Answer" />
+    <div class="question-box-actions">
+      <!-- Legg til bildet og sletteknappen her -->
+    </div>
+  </div>
+</template>
+
+<script setup>
+// Logikk for å håndtere spørsmål og svar samt handlinger
+</script>
+
+<style scoped>
+.question-box {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 10px;
+  background-color: white;
+  border: black 1px solid;
+}
+</style>
