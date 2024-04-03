9e9a818dfdb8832f22aac5a61df4a44060251100
andreaamundsen <andramun@stud.ntnu.no>
2024-04-03 08:26:55 +0200

    set integration test to us H2 database

--- backend/quizbank/src/test/java/no/ntnu/idatt2105/quizbank/integrationTests/AuthControllerIntegrationTest.java
+++ backend/quizbank/src/test/java/no/ntnu/idatt2105/quizbank/integrationTests/AuthControllerIntegrationTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
 
@@ -19,8 +20,10 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
+
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class AuthControllerIntegrationTest {
 
     @Autowired
