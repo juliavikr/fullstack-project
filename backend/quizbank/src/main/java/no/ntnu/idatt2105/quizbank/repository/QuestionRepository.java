859117d6d2ad5137d9677f1030936fd2b61e7824
andreaamundsen <andramun@stud.ntnu.no>
2024-04-06 01:23:25 +0200

    fixes

--- backend/quizbank/src/main/java/no/ntnu/idatt2105/quizbank/repository/QuestionRepository.java
+++ backend/quizbank/src/main/java/no/ntnu/idatt2105/quizbank/repository/QuestionRepository.java
@@ -4,10 +4,12 @@ import no.ntnu.idatt2105.quizbank.model.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
   // Find a question by its ID
-  Question findById(long id);
+    Optional<Question> findById(Long id);
 
   // Delete a question by its ID
   void deleteById(long id);
