717dcf5fa8770a81a64310509bd6a9c7c2dd9220
andreaamundsen <andramun@stud.ntnu.no>
2024-04-07 08:08:33 +0200

    add tests, controller tests fails

--- backend/quizbank/src/test/java/no/ntnu/idatt2105/quizbank/repositoryTests/QuestionRepositoryTests.java
+++ backend/quizbank/src/test/java/no/ntnu/idatt2105/quizbank/repositoryTests/QuestionRepositoryTests.java
@@ -3,14 +3,11 @@ package no.ntnu.idatt2105.quizbank.repositoryTests;
 import no.ntnu.idatt2105.quizbank.model.Question;
 import no.ntnu.idatt2105.quizbank.model.Quiz;
 import no.ntnu.idatt2105.quizbank.repository.QuestionRepository;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
