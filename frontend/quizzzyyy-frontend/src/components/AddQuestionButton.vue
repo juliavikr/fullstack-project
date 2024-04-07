225cd6e164385f4f564fb70b30cfeef5c2f1894d
andreaamundsen <andramun@stud.ntnu.no>
2024-04-07 14:56:56 +0200

    frontend improvement quiz creation

--- frontend/quizzzyyy-frontend/src/components/AddQuestionButton.vue
+++ frontend/quizzzyyy-frontend/src/components/AddQuestionButton.vue
@@ -1,6 +1,7 @@
 <template>
-  <button class="add-question-button" @click="addQuestion">+</button>
+  <button class="primary-button add-question-button" @click="addQuestion"> + </button>
 </template>
+
 
 <script setup>
 const emit = defineEmits(['click'])
@@ -14,11 +15,11 @@ const addQuestion = () => {
 .add-question-button {
   background: #ff8bc3;
   border-radius: 50%;
+  margin: 20px;
+  padding: 10px;
   font-size: 2rem;
   width: 60px;
   height: 60px;
-  background: #ff8bc3;
-  margin: 20px;
   border: 1px solid black;
 }
 </style>
